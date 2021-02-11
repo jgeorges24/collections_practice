5a70087cfdc28ee9db74ed171c612cf8eea322e9
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-10 20:05:06 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
 
+
+
+def sort_array_asc(array)
+    return array.sort
+    
+end
+
+def sort_array_desc(array)
+    return array.sort.reverse
+end
+
+
+def sort_array_char_count(array)
+    array.sort_by {|x| x.length } 
+
+end
+
+def swap_elements(array)
+    array.collect {|x| x }
+
+end
